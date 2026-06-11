e917a07689a5a5ab9e72f268186c1cdc6659d973
Bestony@Homelab <bestony@linux.com>
2026-06-11 16:11:26 +0800

    style(docs): simplify Starlight theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,28 +6,29 @@ import starlightVersions from 'starlight-versions';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-      starlight({
-          title: '人人都能学会的 WordPress 实战课',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/bestony/EasyWordPressBook' }],
-          sidebar: [
-              {
-                  label: 'Guides',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Example Guide', slug: 'guides/example' },
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  items: [{ autogenerate: { directory: 'reference' } }],
-              },
+    starlight({
+      title: '人人都能学会的 WordPress 实战课',
+      customCss: ['/src/styles/gitbook.css'],
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/bestony/EasyWordPressBook' }],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
           ],
-          plugins: [
-              starlightVersions({
-                  current: { label: '最新版', redirect: 'root' },
-                  versions: [{ slug: 'legacy', label: 'Legacy', redirect: 'root' }],
-              }),
-          ],
-      }),
-	],
+        },
+        {
+          label: 'Reference',
+          items: [{ autogenerate: { directory: 'reference' } }],
+        },
+      ],
+      plugins: [
+        starlightVersions({
+          current: { label: '最新版', redirect: 'root' },
+          versions: [{ slug: 'legacy', label: 'Legacy', redirect: 'root' }],
+        }),
+      ],
+    }),
+  ],
 });
